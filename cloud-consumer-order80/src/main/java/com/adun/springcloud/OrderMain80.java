03c9ec800f4b5c45c7bcd04af08a57913bdb1063
zhudunfeng <2101580047@qq.com>
2022-02-28 15:38:07 +0800

    Ribbon核心组件IRule：指定负载均衡算法，可全局替换、可单服务指定

--- cloud-consumer-order80/src/main/java/com/adun/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/adun/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.adun.springcloud;
 
+import com.adun.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author Zhu Dunfeng
@@ -10,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+//自定义ribbon负载均衡算法
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
