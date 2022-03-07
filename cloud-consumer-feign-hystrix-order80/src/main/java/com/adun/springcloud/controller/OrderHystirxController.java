5cbcc48c28fa8ea037569b57af513a2928778a7e
zhudunfeng <2101580047@qq.com>
2022-03-07 21:10:12 +0800

    消费者进行hystrix服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/adun/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/adun/springcloud/controller/OrderHystirxController.java
@@ -1,6 +1,8 @@
 package com.adun.springcloud.controller;
 
 import com.adun.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,19 +22,26 @@ public class OrderHystirxController {
     private PaymentHystrixService paymentHystrixService;
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id)
-    {
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_OK(id);
-        log.info("****result: "+result);
+        log.info("****result: " + result);
         return result;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) throws InterruptedException
-    {
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) throws InterruptedException {
+        //int age=10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
-        log.info("****result: "+result);
+        log.info("****result: " + result);
         return result;
     }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
 
 }
