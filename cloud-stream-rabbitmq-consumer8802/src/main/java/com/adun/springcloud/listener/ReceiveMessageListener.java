ec2463dbf7d621567da68228202fcdd40e8ee1eb
zhudunfeng <2101580047@qq.com>
2022-03-13 19:02:35 +0800

    stream消息output、input、重复消费（分组）、实例宕机继续消费（分组）

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/adun/springcloud/listener/ReceiveMessageListener.java
@@ -0,0 +1,27 @@
+package com.adun.springcloud.listener;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Zhu Dunfeng
+ * @date 2022/3/13 18:21
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message)
+    {
+        System.out.println("消费者1号，------->接收到的消息：" + message.getPayload()+"\t port: "+serverPort);
+    }
+
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/adun/springcloud/service/IMessageProvider.java
@@ -0,0 +1,6 @@
+package com.adun.springcloud.service;
+
+public interface IMessageProvider
+{
+    public String send() ;
+}
