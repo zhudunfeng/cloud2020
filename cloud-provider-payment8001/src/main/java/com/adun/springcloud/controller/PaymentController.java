6e5ca910795bdfdede55ee4c882aac423df3ab72
zhudunfeng <2101580047@qq.com>
2022-03-14 22:58:42 +0800

    引入SpringCloud Sleuth

--- cloud-provider-payment8001/src/main/java/com/adun/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/adun/springcloud/controller/PaymentController.java
@@ -82,5 +82,11 @@ public class PaymentController {
         return port;
     }
 
+    @GetMapping("/zipkin")
+    public String paymentZipkin()
+    {
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
+    }
+
 
 }
